9f0b77ad43dedbd33cab242de487cc306ea995a8
Hocine Cauchoix <hocine.cauchoix@ynov.com>
2024-02-17 02:07:27 +0100

    Nav page panier + redirection

--- pages/panier.php
+++ pages/panier.php
@@ -1,14 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Marker+Hatch&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fascinate+Inline&family=Rubik+Marker+Hatch&family=Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/product.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Permanent+Marker&family=Whisper&display=swap" rel="stylesheet">
+    <title>Navbar and Cards</title>
 </head>
-<body>
-    
-</body>
-</html>
+
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./catalogue.php">PHP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto" style="font-size: 20px;"> 
+                <li class="nav-item active">
+                    <a class="nav-link" href="./catalogue.php">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./profil.php">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./basket.php">Basket</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./veste.php">Vestes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pantalon.php">Pantalon</a>
+                </li>
+            </ul>
+            <?php
+            if (isset($_SESSION['user_id'])) {
+                echo '<a href="' . $panier_url . '" class="btn btn-primary ml-2">Mon Panier <span class="badge badge-light">X</span></a>';
+            }
+            ?>
+        </div>
+    </div>
+</nav>
 
 <?php
 session_start();
@@ -57,3 +98,4 @@ if (isset($_SESSION['user_id'])) {
     echo 'Utilisateur non connecté.';
 }
 ?>
+
--- pages/veste.php
+++ pages/veste.php
@@ -54,8 +54,8 @@ $result = $conn->query($sql);
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">PHP</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="./catalogue.php">PHP</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
